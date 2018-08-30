4cfe373e747eee0531554273e8cb914c8167b52d
Samas <you@example.com>
2018-08-30 11:49:53 +0530

    commit

---
+++ JavaWorks/src/com/exilant/day1/TestEquals.java
@@ -0,0 +1,15 @@
+package com.exilant.day1;
+
+public class TestEquals {
+	
+	public static void main(String[] args) {
+		
+		String s="samas";
+		String s1="samas";
+		Customer cus=new Customer();
+		Customer cus1=new Customer();
+		System.out.println(s.equals(s1));
+		System.out.println(cus.equals(cus1));
+	}
+
+}
